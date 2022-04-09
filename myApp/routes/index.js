24a0294b89714bfebc17aa91aef395ec692f1feb
nachoojeda <iojedavecellio@udesa.edu.ar>
2022-04-09 19:34:35 -0300

    hice el sistema de ruteo de index y de users. Empece con los registros y log in

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -1,9 +1,33 @@
+const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/error', function(req, res, next) {
+  res.render('error', { title: 'Express' });
+});
+
+router.get('/headerLogueado', function(req, res, next) {
+  res.render('headerLogueado', { title: 'Express' });
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/product-add', function(req, res, next) {
+  res.render('product-add', { title: 'Express' });
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('product', { title: 'Express' });
+});
+
+router.get('/search-results', function(req, res, next) {
+  res.render('search-results', { title: 'Express' });
+});
+
 
 module.exports = router;
--- myApp/routes/users.js
+++ myApp/routes/users.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/account', function(req, res, next) {
+  res.render('account', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/profile-edit', function(req, res, next) {
+  res.render('profile-edit', { title: 'Express' });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Express' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
+
 module.exports = router;
