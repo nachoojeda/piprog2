6ccae068c023b0419f14f4f0cd1e9ec1a9622c75
francoferrero22 <ferrerof@udesa.edu.ar>
2022-04-11 09:23:32 -0300

    contro

--- myApp/controllers/profileController.js
+++ myApp/controllers/profileController.js
@@ -1,8 +1,17 @@
-const zapatillas = require('../db')
+const guitarras = require('../db')
 const controller = {
     index: function (req,res) {
         let iguitarras = guitarras
-        
-        
-    }
+res.render('index', {guitarras})
+    },
+  login: function (req,res) {
+      res.render('login');
+  } ,
+  register:function (req,res) {
+      res.render('register')
+      
+  },
+  profile: function (req,res) {
+      res.render('profile')
+  }
 }
