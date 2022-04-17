7ec525c73f6828d18b7afd2010a8297455eb0069
Segundo Benito <segundobenito@Segundos-MacBook-Pro.local>
2022-04-17 12:32:51 -0300

    .

--- myApp/controllers/profileController.js
+++ myApp/controllers/profileController.js
@@ -4,18 +4,18 @@ const controller = {
     index: function (req, res) {
         res.render('index', {db})
     },
-    login: function(req, res, next) {
+    login: function(req, res,) {
         res.render('login', { title: 'Express' });
       
     },
-    register: function(req, res, next) {
+    register: function(req, res,) {
         res.render('register', { title: 'Express' });
     },
-    profile: function(req, res, next) {
+    profile: function(req, res,) {
         res.render('profile', { title: 'Express' } + db.profile);
     
     },
-    profileEdit : function(req, res, next) {
+    profileEdit : function(req, res,) {
         res.render('profile-edit', { title: 'Express' });
       },
 }
