6027e3372b0492b40d474b5980c9a7896df432a2
Segundo Benito <segundobenito@Segundos-MacBook-Pro.local>
2022-04-17 17:36:24 -0300

    Search y resultados configuraciones

--- myApp/controllers/profileController.js
+++ myApp/controllers/profileController.js
@@ -4,20 +4,6 @@ const controller = {
     index: function (req, res) {
         res.render('index', {productos: db.products,comments: db.comentarios}) 
     },
-<<<<<<< HEAD
-    login: function(req, res,) {
-        res.render('login', { title: 'Express' });
-      
-    },
-    register: function(req, res,) {
-        res.render('register', { title: 'Express' });
-    },
-    profile: function(req, res,) {
-        res.render('profile', { title: 'Express' } + db.profile);
-    
-    },
-    profileEdit : function(req, res,) {
-=======
     login: function(req, res) {
         res.render('login'); 
     },
@@ -28,7 +14,6 @@ const controller = {
         res.render('profile', {profile: db.profile,productos: db.products,comments: db.comentarios});
     },
     profileEdit : function(req, res) {
->>>>>>> 69a0961348856d163622ca3fece9af72f85ed20d
         res.render('profile-edit', { title: 'Express' });
       },
 }
